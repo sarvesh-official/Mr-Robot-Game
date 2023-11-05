4f68179ea461c3c9063f4b9d828e09988f9e423e
sarvesh-official <sarveshjaiofficial@gmail.com>
2023-11-06 01:02:48 +0530

    added typing script

--- Pages/Game/typing.js
+++ Pages/Game/typing.js
@@ -11,22 +11,103 @@ function randomWord() {
   return words[randomIndex - 1];
 }
 
+// Adding classname to current elements
+function addClass(el, name) {
+  el.className += " " + name;
+}
+// Removing classname to current elements
+
+function removeClass(el, name) {
+  el.className = el.className.replace(name, "");
+}
+
 // Formatting the Words
 
 function formatWord(word) {
-  // span tag is opened twice because the first letter has only closing tag
-  return `<div class="word">
-            <span class="letter">${word
-              .split("")
-              .join('</span><span class="letter">')}
-              </span>
-              </div>`;
+  let formattedWord = "<div class='word'>";
+
+  word.split("").forEach((letter) => {
+    formattedWord += `<span class="letter">${letter}</span>`;
+  });
+
+  formattedWord += "</div>";
+  return formattedWord;
 }
+// Testing
+// function formatWord(word) {
+//   let formattedWord = `<div class="word">`;
+//   word.split("").forEach((letter) => {
+//     formattedWord += `<span class="letter">${letter}</span>`;
+//   });
+//   formattedWord += `</div>`;
+//   return formattedWord;
+// }
+
 // New Game
 function newGame() {
   document.getElementById("words").innerHTML = "";
   for (let i = 0; i < 200; i++) {
     document.getElementById("words").innerHTML += formatWord(randomWord());
   }
+  addClass(document.querySelector(".word"), "current");
+  addClass(document.querySelector(".letter"), "current");
 }
+
+// Listening to Keys
+
+document.getElementById("game").addEventListener("keyup", (ev) => {
+  const key = ev.key;
+  const currentLetter = document.querySelector(".letter.current");
+  // Optional Changing Operator ?
+  const expected = currentLetter?.innerHTML || " ";
+  const currentWord = document.querySelector(".word.current");
+  const isLetter = key.length === 1 && key != " ";
+  const isSpace = key === " ";
+
+  console.log({ key, expected });
+
+  if (isLetter) {
+    if (currentLetter) {
+      addClass(currentLetter, key == expected ? "correct" : "incorrect");
+      removeClass(currentLetter, "current");
+      if ((currentLetter.nextSibling, "current")) {
+        addClass(currentLetter.nextSibling, "current");
+      }
+    }
+    // If extra letters are entered by the user it is counted as extra
+    else {
+      const incorrectLetter = document.createElement("span");
+      incorrectLetter.innerHTML = key;
+      incorrectLetter.className = "letter incorrect extra";
+      currentWord.append(incorrectLetter);
+    }
+  }
+
+  if (isSpace) {
+    if (expected !== " ") {
+      // Selecting the whole word and making the letters red
+      const lettersToInvalidate = [
+        ...document.querySelectorAll(".word.current .letter:not(.correct)"),
+      ];
+      lettersToInvalidate.forEach((letter) => {
+        addClass(letter, "incorrect");
+      });
+    }
+    removeClass(currentWord, "current");
+    addClass(currentWord.nextSibling, "current");
+    if (currentLetter) {
+      removeClass(currentLetter, "current");
+    }
+    addClass(currentWord.nextSibling.firstChild, "current");
+  }
+
+  // move the cursor
+  const nextLetter = document.querySelector(".letter.current");
+  const cursor = document.getElementById("cursor");
+  if (nextLetter) {
+    cursor.style.top = nextLetter.getBoundingClientRect().top;
+    cursor.style.left = nextLetter.getBoundingClientRect().left;
+  }
+});
+
 newGame();
